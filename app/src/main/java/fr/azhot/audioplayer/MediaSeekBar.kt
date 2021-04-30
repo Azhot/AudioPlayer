b80ba3065285e1f3fdd5e070251f1948a9bc6fc9
Azhot <francois.jouvelot@gmail.com>
2021-04-30 14:37:27 +0200

    Added MediaSeekBar class Implemented media controller fragment layout

---
+++ app/src/main/java/fr/azhot/audioplayer/MediaSeekBar.kt
@@ -0,0 +1,8 @@
+package fr.azhot.audioplayer
+
+import android.content.Context
+import androidx.appcompat.widget.AppCompatSeekBar
+
+class MediaSeekBar constructor(context: Context) : AppCompatSeekBar(context) {
+
+}
